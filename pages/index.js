bfacd0d7e3d349959b57451983757ccf24fa71cc
Kelly Davis <kelly.davis@forestry.io>
2022-02-01 20:30:29 -0500

    add popup when navigating to a site

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 import {staticRequest} from "tinacms";
 import {Layout} from "../components/Layout";
 import {useTina} from "tinacms/dist/edit-state";
@@ -21,6 +21,8 @@ const query = `{
 }`;
 
 export default function Home(props) {
+    const [selectedSite, setSelectedSite] = useState('');
+
     // data passes though in production mode and data is updated to the sidebar data in edit-mode
     const {data} = useTina({
         query,
@@ -29,22 +31,25 @@ export default function Home(props) {
     });
 
     const bookmarksList = data.getBookmarksList.edges;
-    const codeToUrl = {}
+    const codeToBookmark = {}
 
     if (data) {
         for (let bookmark of bookmarksList) {
             if (bookmark.node.data.keyBinding) {
-                codeToUrl[bookmark.node.data.keyBinding] = bookmark.node.data.url
+                codeToBookmark[bookmark.node.data.keyBinding] = bookmark.node.data
             }
         }
     }
 
     useEffect(function onFirstMount() {
         function handleOnKeyDown(evt) {
-            const url = codeToUrl[evt.key]
-            if (url) {
-                // TODO flash a confirmation here
-                window.location.href = url
+            const data = codeToBookmark[evt.key]
+            if (data) {
+                const url = data.url
+                if (url) {
+                    window.location.href = url
+                }
+                setSelectedSite(evt.key)
             }
         }
 
@@ -78,6 +83,27 @@ export default function Home(props) {
                     </div>
                 </div>
             </div>
+            {
+                selectedSite !== '' && (
+                    <div
+                        className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+                    >
+                        <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t bg-slate-200">
+                                    <h3 className="text-3xl">
+                                        navigating to <span className="font-semibold">{codeToBookmark[selectedSite].title}</span>
+                                    </h3>
+                                </div>
+                                <div className="relative p-6 flex-auto bg-slate-300">
+                                    <img src="https://tina.io/img/rico-replacement.jpg"></img>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
+
         </Layout>
     );
 }
