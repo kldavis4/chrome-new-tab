0614a009fc6c36f29ad23c5b06c77f93e4046177
Kelly Davis <kelly.davis@forestry.io>
2022-02-02 11:29:50 -0500

    make entire tile clickable

--- pages/index.js
+++ pages/index.js
@@ -66,27 +66,30 @@ export default function Home(props) {
 
     return (
         <Layout>
-            <div className="invisible"><form><input type="text" ref={textInput} /></form></div>
+            <div className="invisible">
+                <form><input type="text" ref={textInput}/></form>
+            </div>
             <div className="bg-white">
                 <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
                     <div
                         className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
                         {bookmarksList.map((bookmark) => (
                             <div key={bookmark.node.id} className="group relative">
-                                <div className="border-solid border-2">
-                                    <p className="text-6xl font-extrabold font-serif group-hover:opacity-75 text-center py-10">
-                                        {bookmark.node.data.site}
-                                    </p>
-                                </div>
-                                <div className="mt-4 flex justify-between">
-                                    <div>
-                                        <h3 className="text-lg text-gray-700">
-                                            <Link href={bookmark.node.data.url}>
-                                                <a>({bookmark.node.data.keyBinding}) {bookmark.node.data.title}</a>
-                                            </Link>
-                                        </h3>
+                                <Link href={bookmark.node.data.url}><a>
+                                    <div className="border-solid border-2">
+                                        <p className="text-6xl font-extrabold font-serif group-hover:opacity-75 text-center py-10">
+                                            {bookmark.node.data.site}
+                                        </p>
                                     </div>
-                                </div>
+
+                                    <div className="mt-4 flex justify-between">
+                                        <div>
+                                            <h3 className="text-lg text-gray-700 text-center">
+                                                ({bookmark.node.data.keyBinding}) {bookmark.node.data.title}
+                                            </h3>
+                                        </div>
+                                    </div>
+                                </a></Link>
                             </div>
                         ))}
                     </div>
@@ -98,10 +101,13 @@ export default function Home(props) {
                         className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
                     >
                         <div className="relative w-auto my-6 mx-auto max-w-3xl">
-                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t bg-slate-200">
+                            <div
+                                className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                                <div
+                                    className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t bg-slate-200">
                                     <h3 className="text-3xl">
-                                        navigating to <span className="font-semibold">{codeToBookmark[selectedSite].title}</span>
+                                        navigating to <span
+                                        className="font-semibold">{codeToBookmark[selectedSite].title}</span>
                                     </h3>
                                 </div>
                                 <div className="relative p-6 flex-auto bg-slate-300">
